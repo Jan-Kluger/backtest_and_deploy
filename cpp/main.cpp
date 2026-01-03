1cf5a76d10b289d96f3cf3271904ab7b1aac8c99
yannick <yannick@schuermann.org>
2026-01-04 00:41:09 +0100

    price fetch cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,30 +2,30 @@
 #include <libpq-fe.h>
 
 int main() {
-    PGconn* conn = PQconnectdb("host=localhost dbname=crypto");
+  PGconn *conn = PQconnectdb("host=localhost dbname=ctrade");
 
-    if (PQstatus(conn) != CONNECTION_OK) {
-        std::cerr << "Connection failed: " << PQerrorMessage(conn) << std::endl;
-        PQfinish(conn);
-        return 1;
-    }
+  if (PQstatus(conn) != CONNECTION_OK) {
+    std::cerr << "Connection failed: " << PQerrorMessage(conn) << std::endl;
+    PQfinish(conn);
+    return 1;
+  }
 
-    PGresult* res = PQexec(conn,
-        "SELECT ts, close FROM btcusdt_1m ORDER BY ts DESC LIMIT 1");
+  PGresult *res =
+      PQexec(conn, "SELECT ts, close FROM btcusdt_1m ORDER BY ts DESC LIMIT 1");
 
-    if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-        std::cerr << "Query failed: " << PQerrorMessage(conn) << std::endl;
-        PQclear(res);
-        PQfinish(conn);
-        return 1;
-    }
-
-    if (PQntuples(res) > 0) {
-        std::cout << PQgetvalue(res, 0, 0) << " " << PQgetvalue(res, 0, 1) << std::endl;
-    }
-
+  if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+    std::cerr << "Query failed: " << PQerrorMessage(conn) << std::endl;
     PQclear(res);
     PQfinish(conn);
-    return 0;
-}
+    return 1;
+  }
 
+  if (PQntuples(res) > 0) {
+    std::cout << PQgetvalue(res, 0, 0) << " " << PQgetvalue(res, 0, 1)
+              << std::endl;
+  }
+
+  PQclear(res);
+  PQfinish(conn);
+  return 0;
+}
